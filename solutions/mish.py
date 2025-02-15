3858ced957a7613f1abe59257880398c99ba5d1a
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-02-15 12:15:16 +0000

    Clean solution

--- solutions/mish.py
+++ solutions/mish.py
@@ -1,26 +1,17 @@
 class Strategy:
     def __init__(self):
-        self.sneaky = False
         self.cooperate_for = 0
 
     def move(self, self_history: list[bool], opponent_history: list[bool]):
         iteration = len(self_history)
         if iteration == 0:
             return True
-        if iteration == 15:
-            # At iteration 15, get bored and try exploiting the opponent
-            # self.sneaky = True
-            pass
         if self.cooperate_for:
             self.cooperate_for -= 1
             return True
-        if self.sneaky:
-            # Try to be sneaky and exploit the opponent
-            return False
         if opponent_history[-1] is False:
-            # Too scared to try to be sneaky
-            self.sneaky = False
-            self.cooperate_for = 2
+            # Get scared and try cooperating for a bit
+            self.cooperate_for = 1
 
         # Tit for tat:
         return opponent_history[-1]
