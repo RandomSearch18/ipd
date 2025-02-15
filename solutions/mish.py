6da7a66d06424548b0509b3f502141fb36c4b470
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-02-15 13:41:19 +0000

    slightly optimise against always deefect

--- solutions/mish.py
+++ solutions/mish.py
@@ -6,15 +6,27 @@ class Strategy:
         self.cooperate_for = 0
 
     def move(self, self_history: list[bool], opponent_history: list[bool]):
+        def is_evil(history: list[bool]) -> bool:
+            if len(history) < 3:
+                return False
+            for i in range(3):
+                if history[i] is True:
+                    return False
+            return True
+
         iteration = len(self_history)
         if iteration == 0:
             return True
-        # opponent_is_evil =
+        opponent_is_evil = is_evil(opponent_history)
+        if opponent_is_evil:
+            # No One Mourns The Wicked
+            # (always defect if opponent looks like an always-defector)
+            return False
         if self.cooperate_for:
             self.cooperate_for -= 1
             return True
         if opponent_history[-1] is False:
-            if random() < 0.1:
+            if random() < 0.15:
                 # Get scared and try cooperating for a bit
                 self.cooperate_for = 2
 
