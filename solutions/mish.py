31b2b62c71df329d33d5caa95b080933b7626678
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-02-15 13:30:59 +0000

    Add a factor of randomness

--- solutions/mish.py
+++ solutions/mish.py
@@ -1,3 +1,6 @@
+from random import random
+
+
 class Strategy:
     def __init__(self):
         self.cooperate_for = 0
@@ -6,12 +9,14 @@ class Strategy:
         iteration = len(self_history)
         if iteration == 0:
             return True
+        # opponent_is_evil =
         if self.cooperate_for:
             self.cooperate_for -= 1
             return True
         if opponent_history[-1] is False:
-            # Get scared and try cooperating for a bit
-            self.cooperate_for = 1
+            if random() < 0.1:
+                # Get scared and try cooperating for a bit
+                self.cooperate_for = 2
 
         # Tit for tat:
         return opponent_history[-1]
